7f5faf1597cd85827c05f9f2e6472d36380f7ca2
neecholaus <thisisnickadam@gmail.com>
2023-01-03 10:13:16 -0800

    feat(trainer-route-group,auth-middleware) auth middleware only enforces that an auth header is provided, does not yet validate that value)

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	server := gin.Default()
@@ -11,5 +13,28 @@ func main() {
 		})
 	})
 
-	server.Run()
+	trainerNS := server.Group("trainer", auth())
+
+	trainerNS.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "You are logged in as a trainer",
+		})
+	})
+
+	_ = server.Run(":80")
+}
+
+func auth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.GetHeader("Authorization")
+		if token != "" {
+			// todo - validate provided token
+		} else {
+			c.Abort()
+			c.JSON(403, gin.H{
+				"error": "You are not logged in",
+			})
+		}
+		c.Next()
+	}
 }
