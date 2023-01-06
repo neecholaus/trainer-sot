83ef31619f4548e9dc4331d628ea3c795edd0bde
neecholaus <thisisnickadam@gmail.com>
2023-01-06 09:52:22 -0800

    docs(element-hydrator)

--- resources/public/js/utilities.js
+++ resources/public/js/utilities.js
@@ -3,9 +3,11 @@ class ElementHydrator {
      * Note:
      * The invoker's `elements` and `container` property must be public.
      *
-     * @param object invoker
+     * @param invoker object
      */
     static hydrate(invoker) {
+
+        // Hydrate container, if applicable, for more narrow element querying
         if (invoker.container !== undefined && typeof(invoker.container) == 'string') {
             invoker.container = this.#getElement(invoker.container, invoker);
         }
@@ -13,6 +15,18 @@ class ElementHydrator {
         this.#hydrateElements(invoker);
     }
 
+    /**
+     * Note:
+     * `nestedMappings` allows for stepping through element groupings, such as
+     * putting all form inputs inside an object keyed by `formInputs`. When absent,
+     * the `invoker's` elements property is used.
+     *
+     * The invoker is passed on to #getElement in order to use the container for
+     * narrow element querying.
+     *
+     * @param invoker object
+     * @param nestedMappings ?object
+     */
     static #hydrateElements(invoker, nestedMappings=null) {
         const mappings = nestedMappings || invoker.elements;
 
@@ -25,6 +39,12 @@ class ElementHydrator {
         }
     }
 
+    /**
+     *
+     * @param selector string
+     * @param invoker object
+     * @return object
+     */
     static #getElement(selector, invoker) {
         if (typeof(invoker.container) !== 'string') {
             return invoker.container.querySelector(selector);
